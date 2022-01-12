c9c84d91cf777aa60f0e35e311064f3a6b0f2e0c
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-12 21:59:21 +0530

    Add files via upload

---
+++ if stmt.cpp
@@ -0,0 +1,16 @@
+//if condition statement
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"enter first number \n";
+	cin>>a;
+	cout<<"enter second number \n";
+	cin>>b;
+	if(a>b){
+		cout<<a<<" is larger";
+	}
+	else{
+		cout<<b<<" is larger";
+	}
+}
