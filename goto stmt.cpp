b2e8f8c300d8a15158cd02928130f58335eceee5
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-12 22:43:34 +0530

    Add files via upload

---
+++ goto stmt.cpp
@@ -0,0 +1,17 @@
+//goto statement
+#include<iostream>
+using namespace std;
+int main(){
+	ineligible:
+		cout<<"you are not eligible to vote"<<"\n";
+	cout<<"enter your age"<<"\n";
+	int age;
+	cin>>age;
+	if(age<18){
+		goto ineligible;
+	}
+	else
+	{
+		cout<<"you can vote";
+	}
+}
