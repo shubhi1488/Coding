2f05ee0f82f0f54a85b20fb3dee8cff15d64fc0c
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-24 16:57:20 +0530

    Add files via upload

---
+++ substr().cpp
@@ -0,0 +1,9 @@
+//substr(int starting pos,int ending pos)
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	string str="Welcome to programming world";
+	string s=str.substr(0,7);
+	cout<<"string part :"<<s;
+}
