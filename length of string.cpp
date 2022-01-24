97b39e5021a7fa66e15a34912f24f377b7b79d0a
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-24 15:49:45 +0530

    Add files via upload

---
+++ length of string.cpp
@@ -0,0 +1,9 @@
+//to find the length of the string
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	char str[]="welcome to GLA University!";
+	cout<<"length of the string is:"<<strlen(str);
+	return 0;
+}
