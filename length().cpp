d87f9776fc5e63409275b1db4fabdbd3f315d054
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-01-24 16:44:00 +0530

    Add files via upload

---
+++ length().cpp
@@ -0,0 +1,10 @@
+//length()
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	string s="Welcome to Gla University";
+	int l=s.length();
+	cout<<"length of the string is:"<<l;
+	return 0;
+}
